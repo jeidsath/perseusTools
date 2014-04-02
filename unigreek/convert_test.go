748e888d9d90bf1108daabb9176b1225165906bb
Joel Eidsath <jeidsath@gmail.com>
2014-04-01 22:21:04 -0700

    Working conversion from bcode

---
+++ unigreek/convert_test.go
@@ -0,0 +1,39 @@
+package unigreek
+
+import (
+	"fmt"
+	"testing"
+)
+
+var TestStrings = [][2]string{
+	[2]string{"a", "α"},
+	[2]string{"abg", "αβγ"},
+	[2]string{"*abg", "Αβγ"},
+	[2]string{"a/bg", "άβγ"},
+	[2]string{"plh\\n *milhsi/wn.", "πλὴν Μιλησίων."},
+	[2]string{"mh=nin a)/eide qea\\ *phlhi+a/dew *)axilh=os", "μῆνιν ἄειδε θεὰ Πηληϊάδεω Ἀχιλῆος"},
+	[2]string{"[*)axilh=os] ", "[Ἀχιλῆος] "},
+	[2]string{"tw=|", "τῷ"},
+	[2]string{"*t*w=|", "Τῼ͂"},
+}
+
+func TestConvert(t *testing.T) {
+	for _, tcase := range TestStrings {
+		bcode := tcase[0]
+		ucode := tcase[1]
+
+		converted, err := Convert(bcode)
+
+		if converted != ucode || err != nil {
+			fmt.Printf(`FAILURE: Conversion of "%s" should return: "%s"` + "\n",
+				bcode, ucode)
+			fmt.Printf(`         Instead returns: "%s"` + "\n", converted)
+
+			if err != nil {
+				fmt.Printf(`         Error value: "%s"` + "\n", err.Error())
+			}
+                        fmt.Printf("\n")
+			t.Fail()
+		}
+	}
+}
