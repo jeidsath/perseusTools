969eaed9d056c0c61aca624fb3e0d86f4f7e3102
Joel Eidsath <jeidsath@gmail.com>
2014-04-22 09:47:45 -0700

    Command lines tool

--- perseusXmlToHtml.go
+++ perseusXmlToHtml.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/jeidsath/unigreek"
+        "io/ioutil"
 
-	_ "github.com/jeidsath/perseusTools/parse"
+        "github.com/jeidsath/perseusTools/parse"
 )
 
+func ConvertFile(fileName string) ([]string, error) {
+	contents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return []string{}, err
+	}
+
+	root, err := parse.GetTextables(contents)
+	if err != nil {
+		return []string{}, err
+	}
+
+	texts, err := root.Document()
+	if err != nil {
+		return []string{}, err
+	}
+
+	return texts, nil
+}
+
 func main() {
-	out, _ := unigreek.Convert("alfa")
-	fmt.Println(out)
+	flag.Parse()
+        fileName := flag.Arg(0)
+	if fileName == "" {
+		fmt.Println("No file specified.")
+		return
+	}
+
+	lines, err := ConvertFile(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		for _, ss := range lines {
+			fmt.Println(ss)
+		}
+	}
 }
--- perseusXmlToHtml_test.go
+++ perseusXmlToHtml_test.go
@@ -3,29 +3,8 @@ package main
 import (
         "fmt"
 	"testing"
-        "io/ioutil"
         "strings"
-        "github.com/jeidsath/perseusTools/parse"
 )
-
-func ConvertFile(fileName string) ([]string, error) {
-        contents, err := ioutil.ReadFile(fileName)
-        if err != nil {
-                return []string{}, err
-        }
-
-        root, err := parse.GetTextables(contents)
-        if err != nil {
-                return []string{}, err
-        }
-
-        texts, err := root.Document()
-        if err != nil {
-                return []string{}, err
-        }
-
-        return texts, nil
-}
 
 func TestInitial(t *testing.T) {
         texts, err := ConvertFile("testData/xen.anab_gk.xml")
